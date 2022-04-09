1cc0c3ac218c4c5325094a418072a5b08e11a70a
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2022-04-09 11:43:34 -0300

    Criação da pagina de edição e configuraçao da pagina de listagem

---
+++ src/pages/EditBook.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <NavBar />
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar";
+import API from "@/services/APIs";
+
+export default {
+  name: "EditBook",
+  components: { NavBar },
+  data() {
+    return {
+      msg: "Criar Livros"
+    };
+  },
+};
+</script>
